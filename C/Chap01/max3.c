9814a584c924cbd7c719cff5e3bf54458a7dd5a4
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-09-19 20:40:22 +0900

    max3 PR
    Chap/max3.c PR done!

--- C/Chap01/max3.c
+++ C/Chap01/max3.c
@@ -3,5 +3,18 @@
  
 int main(){
 	
+	int a, b, c;
+	int max;
+	printf("세 정수의 최댓값을 구합니다.\n");
+	printf("a의 값 : "); scanf("%d", &a);
+	printf("b의 값 : "); scanf("%d", &b);
+	printf("c의 값 : "); scanf("%d", &c);
+	
+	max = a; //최댓값을 a로 지정함.  
+	if(max < b) max = b; //만약 b의 값이 최댓값보다 크면 max에 b를 대입함. 
+	if(max < c) max = c;
+	
+	printf("세 정수의 최댓값은 %d이다.", max);
+	
 	return 0;
 }
