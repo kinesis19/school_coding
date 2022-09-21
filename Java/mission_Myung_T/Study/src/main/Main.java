597029ef0b7bbe13ea1aa79ebf69e6281f21a9ad
Kinesis <lordkinesis@naver.com>
2022-09-21 10:53:21 +0900

    리펙토링
    리펙토링

---
+++ Java/mission_Myung_T/Study/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import pq_122.Pq_122;
+
+public class Main {
+	public static void main(String[] args) {
+		Pq_122 pq_122 = new Pq_122();
+		pq_122.run();
+	}
+}
--- Java/mission_Myung_T/Study/src/pq_122/Pq_122.java
+++ Java/mission_Myung_T/Study/src/pq_122/Pq_122.java
@@ -1,11 +1,9 @@
-package pq_122.pg1;
+package pq_122;
 
 import java.util.Scanner;
 
-public class Operator {
-
-	public static void main(String[] args) throws Exception {
-		
+public class Pq_122 {	
+	public void run() {
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("두 개의 수를 띄어쓰기하여 입력해주세요 : ");
@@ -18,7 +16,6 @@ public class Operator {
 		int result = num1_Int > num2_Int ? num1_Int : (num1_Int == num2_Int ? 0 : num2_Int);
 		
 		System.out.print("입력한 값 중 큰 수는 " + result + " 입니다.");
-
 	}
 
 }
