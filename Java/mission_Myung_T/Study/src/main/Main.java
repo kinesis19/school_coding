335796d32b2de35448f1ed7561079040cc25e5a5
Kinesis <lordkinesis@naver.com>
2022-09-22 16:49:17 +0900

    로또 코드 완성
    pq130

--- Java/mission_Myung_T/Study/src/main/Main.java
+++ Java/mission_Myung_T/Study/src/main/Main.java
@@ -3,12 +3,14 @@ package main;
 //import pq_122.Pq_122;
 //import pq_123.Pq_123;
 import pq_125.ForMoonNumPrinter;
+import pq_130.Lotto;
 
 public class Main {
 	public static void main(String[] args) {
 //		Pq_122 pq_122 = new Pq_122();
 //		Pq_123 pq_123 = new Pq_123();
-		ForMoonNumPrinter pq_125 = new ForMoonNumPrinter();
-		pq_125.run();
+//		ForMoonNumPrinter pq_125 = new ForMoonNumPrinter();
+		Lotto pq_130 = new Lotto();
+		pq_130.run();
 	}
 }
---
+++ Java/mission_Myung_T/Study/src/pq_130/Lotto.java
@@ -0,0 +1,18 @@
+package pq_130;
+
+public class Lotto {
+
+	public void run() {
+		int ball_count = 1;
+		System.out.print("로또 번호는 : ");
+		while(ball_count <= 6) { //로또는 6개 공을 뽑음.
+			int ball_num = (int) (Math.random() * 45) + 5; // 로또는 45번까지 있음. random() * 45는 40.5까지 나오므로,
+			// 45까지 나오기 위해 +5를 해줌.
+			ball_count++;
+			System.out.print(ball_num + " ");
+		}
+		System.out.println("입니다.");
+
+	}
+
+}
