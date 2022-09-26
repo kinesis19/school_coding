afc01f703669e4566c15bc9264ea8f3e7ca11fee
Kinesis <lordkinesis@naver.com>
2022-09-27 08:37:08 +0900

    q_10 문제 해결 완료.
    q_10 문제 해결 완료.

--- C/Chap01/practice01/q10_sum.c
+++ C/Chap01/practice01/q10_sum.c
@@ -1,18 +1,20 @@
-/* 1, 2,~~~ n까지의 합을 구합니다 (do문에서 양의 정수만을 n값으로 입력합니다. */
+/* b-a를 출력하는 PR (단, b의 값이 a이하면 b값을 재입력.)*/
 #include <stdio.h>
 
 int main(){
-	int i, n;
+	int a, b;
 	int sum = 0;
-	do{
-		printf("n값 : ");
-		scanf("%d", &n);
-	}while(n<=0);
-	for(i = 1; i <= n; i++){
-		sum += i;
-	}
 	
-	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+	do{
+		printf("a의 값 : ");
+		scanf("%d", &a);
+		printf("b의 값 : ");
+		scanf("%d", &b); 
+	}while(a>=b);
+	
+	sum = b-a;
+	printf("b-a는 %d입니다.", sum);
+	
 	getch();
 	
 	return 0;
