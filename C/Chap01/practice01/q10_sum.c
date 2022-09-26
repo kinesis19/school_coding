e987afdab60d2db0a2b65424eb43ce2d31fda0f2
Kinesis <lordkinesis@naver.com>
2022-09-27 08:30:41 +0900

    q_10 파일만 생성함
    나중에 내부 코드 수정해야 함.

---
+++ C/Chap01/practice01/q10_sum.c
@@ -0,0 +1,21 @@
+/* 1, 2,~~~ n까지의 합을 구합니다 (do문에서 양의 정수만을 n값으로 입력합니다. */
+#include <stdio.h>
+
+int main(){
+	int i, n;
+	int sum = 0;
+	do{
+		printf("n값 : ");
+		scanf("%d", &n);
+	}while(n<=0);
+	for(i = 1; i <= n; i++){
+		sum += i;
+	}
+	
+	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+	getch();
+	
+	return 0;
+}
+
+ 
