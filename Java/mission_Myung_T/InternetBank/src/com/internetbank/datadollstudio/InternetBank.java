887805d0ed22613dec6f1fee3d923721ab7fdbb2
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-09-26 19:16:07 +0900

    [과제] 인뱅 Scanner-nextLine() 2번 호출 해결
    nextLine() 특성 상 값을 입력하고, 엔터를함.
    이때, 입력 받은 값과 엔터(값)가 switch 문에서 사용되었기에 2번 호출 되었던 거임.
    
    이를 방지하기 위해 Integer.parseInt(sc.nextLine()) 와 같이 parse를 사용해 '논리 오류'를 해결함.
    
    과제 완료.
    
    야호~!

--- Java/mission_Myung_T/InternetBank/src/com/internetbank/datadollstudio/InternetBank.java
+++ Java/mission_Myung_T/InternetBank/src/com/internetbank/datadollstudio/InternetBank.java
@@ -7,29 +7,30 @@ public class InternetBank {
 	public static void main(String[] args) throws Exception{
 		
 		boolean run = true;
-		Scanner sr = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int userMoney = 0; // 사용자의 잔고 
 		
 		while(run) {
-//			System.out.print("\n");
-			System.out.println("\n--------------------------------------");
+			System.out.print("\n");
+			System.out.println("--------------------------------------");
 			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료.");
 			System.out.println("--------------------------------------");
 			System.out.print("선택> ");
 			
-			String userAnswer = sr.nextLine();
-			
+			// Scanner-nextInt()로 받는 로직.
+//			int userAnswer = sc.nextInt();
+//			
 //			switch(userAnswer) {
 //			case 1:
 //				System.out.print("예금액> ");
-//				int deposit = sr.nextInt();
+//				int deposit = sc.nextInt();
 //				userMoney = userMoney + deposit;
 //				System.out.print("예금 성공.");
 //				continue;
 //			case 2:
 //				System.out.print("출금액> ");
-//				int withdraw  = sr.nextInt();
+//				int withdraw  = sc.nextInt();
 //				if(0 <= userMoney - withdraw) {
 //					userMoney = userMoney - withdraw ;
 //					System.out.print("출금 성공.");
@@ -48,16 +49,38 @@ public class InternetBank {
 //				continue;
 //			}
 			
+			
+			// 문제에서 제시한 Scanner의 nextLine()를 사용한 로직.
+			String userAnswer = sc.nextLine();
 			switch(userAnswer) {
 			case "1":
 				System.out.print("예금액> ");
-				int deposit = sr.nextInt();
+				int deposit = Integer.parseInt(sc.nextLine());
+				 /* [기존]
+				  * 1. '선택' 에서 원하는 번호(1) 입력 후 엔터.
+				  * 2. 예금액을 '123'으로 입력 후 엔터.
+				  * 3. userMoney에 2에서 입력한 '123'이 들어가고 "예금 성공." 출력됨.
+				  * 4. 2에서 '123'을 입력하고 남은 엔터가 '선택'의 값으로 받음.
+				  * 5. 따라서 엔터를 값으로 받은 '선택'이 한번 호출되고,
+				  * 이때, 엔터의 값은 switch ~ case, default 문에서 처리되므로 또 한번 호출됨.
+				  * 
+				  * [해결 방안]
+				  * 입금액, 출금액에서 Scanner-nextLint()으로 입력 받을 때, 
+				  * 바깥 쪽에 Integer.parseInt()로 묶어서 받음.
+				  * 
+				  * [결과]
+				  * 
+				  * <기존>의 실행결과 '5'와 같이 '선택' 메시지가 2번 호출 되지 않고,
+				  * 프로그래머가 기획한 바와 같이 1번 호출되므로 정상작동 됨.
+				  * 
+				  * 
+				  * */
 				userMoney = userMoney + deposit;
 				System.out.println("예금 성공.");
 				continue;
 			case "2":
 				System.out.print("출금액> ");
-				int withdraw  = sr.nextInt();
+				int withdraw  = Integer.parseInt(sc.nextLine());
 				if(0 <= userMoney - withdraw) {
 					userMoney = userMoney - withdraw ;
 					System.out.println("출금 성공.");
@@ -78,7 +101,7 @@ public class InternetBank {
 			
 		}
 		
-		sr.close();
+		sc.close();
 	}
 
 }
