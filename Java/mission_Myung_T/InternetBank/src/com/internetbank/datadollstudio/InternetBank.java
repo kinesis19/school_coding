da6067b9639e5cb05e4fccf1c1ba9c3174c0fe01
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-09-26 18:53:39 +0900

    [과제] 인뱅 - nextLine(), nextInt() 차이점
    차이점 확인 중.
    문제 : Scanner-nextLine() 사용
    실행 결과 : 예금, 출금액 작성 후 엔터시 -> 선택 인터페이스 2번 실행됨.

--- Java/mission_Myung_T/InternetBank/src/com/internetbank/datadollstudio/InternetBank.java
+++ Java/mission_Myung_T/InternetBank/src/com/internetbank/datadollstudio/InternetBank.java
@@ -12,19 +12,70 @@ public class InternetBank {
 		int userMoney = 0; // 사용자의 잔고 
 		
 		while(run) {
-			System.out.println("--------------------------------------");
+//			System.out.print("\n");
+			System.out.println("\n--------------------------------------");
 			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료.");
 			System.out.println("--------------------------------------");
 			System.out.print("선택> ");
 			
-			int userAnswer = sr.nextInt();
+			String userAnswer = sr.nextLine();
+			
+//			switch(userAnswer) {
+//			case 1:
+//				System.out.print("예금액> ");
+//				int deposit = sr.nextInt();
+//				userMoney = userMoney + deposit;
+//				System.out.print("예금 성공.");
+//				continue;
+//			case 2:
+//				System.out.print("출금액> ");
+//				int withdraw  = sr.nextInt();
+//				if(0 <= userMoney - withdraw) {
+//					userMoney = userMoney - withdraw ;
+//					System.out.print("출금 성공.");
+//				}else {
+//					System.out.print("출금 금액 부족.");
+//				}
+//				continue;
+//			case 3:
+//				System.out.print("잔고>" + userMoney);
+//				continue;
+//			case 4:
+//				System.out.print("프로그램 종료");
+//				run = false;
+//				break;
+//			default:
+//				continue;
+//			}
 			
 			switch(userAnswer) {
-			case 1:
-				
+			case "1":
+				System.out.print("예금액> ");
+				int deposit = sr.nextInt();
+				userMoney = userMoney + deposit;
+				System.out.println("예금 성공.");
+				continue;
+			case "2":
+				System.out.print("출금액> ");
+				int withdraw  = sr.nextInt();
+				if(0 <= userMoney - withdraw) {
+					userMoney = userMoney - withdraw ;
+					System.out.println("출금 성공.");
+				}else {
+					System.out.println("출금 금액 부족.");
+				}
+				continue;
+			case "3":
+				System.out.print("잔고>" + userMoney);
+				continue;
+			case "4":
+				System.out.println("프로그램 종료");
+				run = false;
+				break;
+			default:
+				continue;
 			}
 			
-			break;
 		}
 		
 		sr.close();
