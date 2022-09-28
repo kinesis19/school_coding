a8f60561f30a656eecc8794facc8431e025c004c
Kinesis <lordkinesis@naver.com>
2022-09-28 12:47:18 +0900

    별찍기 완성! - for문 3개
    for문 3개로 인터넷 검색 X
    스스로 해결함.
    for문 2개로 가능한지 연구해보자.
    화이팅!

--- Java/mission_Myung_T/Study/src/pq_126/PrintToStar.java
+++ Java/mission_Myung_T/Study/src/pq_126/PrintToStar.java
@@ -7,14 +7,19 @@ public class PrintToStar {
 		
 		int cnt = 9;
 		int space = 4;		
+		int print = 1;
 		
-		for(int i = 0; i <= cnt; i++) { // y축
+		for(int i = 0; i < cnt; i++) { // y축
 			for(int j = 0; j <= cnt; j++) { // x축
 				System.out.print(" ");
-				if(j == space) {
-					System.out.print("*");
+				if(space == j) {
+					for(int p = 0; p < print; p++) {
+						System.out.print("*");
+					}
+					print+=2;
 					space--;
-				} 
+					System.out.print("\n");
+				}
 			}
 			System.out.print("\n");
 			
