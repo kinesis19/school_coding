4039ef5048f8d55b0112e8e35e21e3dac3ce6112
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-10-06 20:29:13 +0900

    Readme.md - pq_126 add

--- Java/mission_Myung_T/Study/src/pq_126/PrintToStar.java
+++ Java/mission_Myung_T/Study/src/pq_126/PrintToStar.java
@@ -10,14 +10,14 @@ public class PrintToStar {
 		for(int y = 1; y <= 9; y++) { // y축
 			for(int x = 1; x <= 9; x++) { // x축
 				if(y == 1) System.out.print(x == 5 ? "*" : " ");
-				if(y == 2 || y == 4 || y == 6 || y == 8){
-					System.out.print("\n\n");
-					break;
-				}
 				if(y == 3) System.out.print(4 <= x && x <= 6 ? "*" : " ");
 				if(y == 5) System.out.print(3 <= x && x <= 7 ? "*" : " ");
 				if(y == 7) System.out.print(2 <= x && x <= 8 ? "*" : " ");
 				if(y == 9) System.out.print(1 <= x && x <= 9 ? "*" : " ");
+				if(y == 2 || y == 4 || y == 6 || y == 8){
+					System.out.print("\n\n");
+					break;
+				}
 			}
 			
 		}
---
+++ Java/mission_Myung_T/Study/src/pq_126/readme.md
@@ -0,0 +1,1 @@
+https://user-images.githubusercontent.com/90908098/194301314-36c70c2d-4a8c-42ad-84ba-faa24f48eb33.png
