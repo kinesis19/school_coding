bddd8f7ba98c3f9aee6ce28199008cbb5a837ad8
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-10-11 21:13:44 +0900

    [진법변환] save

--- C/Chap01/mission_Ju_Teacher/JinbupChanger.c
+++ C/Chap01/mission_Ju_Teacher/JinbupChanger.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-void opear(int a, int b);
+void opear(int j, int n);
 int main(){
 	int iJ = 0, iN = 0; /* iJ = 진법 입력, iN = 변환할 넘버 입력 */
-	
 	do{
 		printf("진법을 입력 (2부터~36까지) : ");
 		scanf("%d", &iJ);
@@ -18,7 +17,7 @@ int main(){
 
 void opear(int j, int n){
 	
-	if(0 < n){
+	if(0 < n){ 
 		opear(j, n/j);
 		printf("%d", n%j);
 	}
