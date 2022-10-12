9444674f075c70b189cb88e0802918795d2f791c
Kinesis <lordkinesis@naver.com>
2022-10-12 10:30:39 +0900

    pq_131 제작 완료.

---
+++ Java/mission_Myung_T/Study/src/pq_131/PcVsPerson.java
@@ -0,0 +1,27 @@
+package pq_131;
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class PcVsPerson {
+
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		Scanner sr = new Scanner(System.in);
+		
+		int pcChoose = rand.nextInt(5)+1;
+		int tryCount = 0;
+		int userAnswer;
+		
+		do {
+			System.out.print("컴퓨터가 생각하는 수를 입력하세요 : ");
+			userAnswer = sr.nextInt();
+			tryCount++;
+			if(userAnswer == pcChoose) System.out.print("축하합니다 " + tryCount + "만에 맞췄습니다.");
+		}while(userAnswer != pcChoose);
+
+	}
+
+}
