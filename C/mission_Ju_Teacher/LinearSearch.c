f64ba7e4722fbc1256ba6c73902b284794037b5d
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-10-25 19:21:02 +0900

    LinearSearch 프로그램 개발 완료.
    인공지능 수학에서는 선형 회귀를 사용하므로 선형 회귀를 배우고 싶은 마음이 있었다.  C언어를 통해 선형 검색을 먼저 배웠으니 계속 공부하면서 선형 회귀도 공부했으면 한다.
    
    (설명란에 소감문을 작성해 버렸네 !)

---
+++ C/mission_Ju_Teacher/LinearSearch.c
@@ -0,0 +1,46 @@
+/* 선형 검색 프로그램Ver.1*/
+#include <stdio.h>
+
+int LinearSearching(int idx_cnt, int find_num, int *x); /* 밑에서 사용할 함수를 미리 선언 함.*/
+
+int main(){
+	int idx_cnt = 0, i, idx_num = 0, find_num = 0; /* idx_cnt : 요소 개수, idx_num = 해당 요소에 넣을 값*/
+	
+	printf("[선형 검색PR]\n");
+	printf("요소 개수 : ");
+	scanf("%d", &idx_cnt); /*요소 개수를 사용자로부터 입력 받음.*/
+	int x[idx_cnt]; /*사용자가 입력한 요소에 알맞은 사이즈의 int 형 배열 x를 선언함.*/
+	for(i=0;i<idx_cnt;i++){ /*for문을 통해 사용자로부터 index값을 입력 받음(0부터 idx_cnt-1까지*/
+		printf("x[%d] : ", i);
+		scanf("%d", &idx_num);
+		x[i] = idx_num;
+	}
+	printf("검색 값 : ");
+	scanf("%d", &find_num); /*사용자로부터 찾을 값을 입력받음*/
+	
+	int result = LinearSearching(idx_cnt, find_num, x); /*리니어 서칭 함수를 통해 나온 리턴 값을 result변수에 넣음.*/
+	
+	if(result != -1){ /*result 변수의 값을 사용해 검색 결과를 출력 혹은 검색 결과가 없음을 출력 함.*/
+		printf("%d은(는)x[%d]에 있습니다.", find_num, result);
+	}else if(result == -1){
+		printf("검색 결과가 없습니다.");
+	}
+	return 0;
+}
+
+int LinearSearching(int idx_cnt, int find_num, int *x){
+	int i;
+	for(i=0;i<find_num;i++){
+//		printf("n번째 시도 값 : %d\n", *&x[i]); /*디버깅 용*/
+		if(*&x[i]==find_num){
+			return i;
+		}else if(i==idx_cnt){
+			return -1;
+		}
+	}
+}
+
+
+
+
+
